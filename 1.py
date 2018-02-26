f5228afafce80f002b94d68b78a84a022f86ae36
Varun Shijo <varunshi@buffalo.edu>
2018-02-26 18:09:41 -0500

    bipartite wip

--- 1.py
+++ 1.py
@@ -1,6 +1,8 @@
 # Author: Varun Shijo UB 50244968
 # DFS for Graphs
 
+flag = "YES"
+
 
 def create_graph():
     vertices, edges = input().split(' ')
@@ -14,27 +16,31 @@ def create_graph():
             graph[left].append(right)
         if left not in graph[right]:
             graph[right].append(left)
-    return graph
+    return graph, vertices
 
 
-def bipartite(graph):
+def dfs(graph, current, visited, color):
     # Traversal
-    root = '1'
-    current = root
-    visited = [current]
-
-    children = [str(_) for _ in sorted(graph[current])]
-
-    while children:
-        if str(current) not in visited:
-            visited.append(children.pop(0))
-        if children:
-            current = children[0]
+    global flag
+    neighbors = [str(_) for _ in graph[current]]
+    if current not in visited:
+        visited.append(current)
+        color[int(current)] = 1 - color[int(current)-1]
+        for neighbor in neighbors:
+            dfs(graph, neighbor, visited, color)
+    elif color[int(current)] == color[int(current) - 1]:
+        flag = "NO"
+    return visited, color
 
 
 def main():
-    graph = create_graph()
-    bipartite(graph)
+    graph, vertices = create_graph()
+    root = '1'
+    color = [0 for _ in range(int(vertices) + 1)]
+    visited, color = dfs(graph, root, [], color)
+    print(visited)
+    print(color)
+    print(flag)
 
 
 if __name__ == '__main__':
