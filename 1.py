87a55efc845af4de03da62ea434b727c6a31e760
Varun Shijo <varunshi@buffalo.edu>
2018-02-26 19:22:45 -0500

    wit's end

--- 1.py
+++ 1.py
@@ -8,7 +8,7 @@ def create_graph():
     vertices, edges = input().split(' ')
 
     # dictionary comprehension to initialize graph nodes
-    graph = {str(_ + 1): [] for _ in range(int(vertices))}
+    graph = {str(_): [] for _ in range(1, int(vertices) + 1)}
     # read from stdin to populate edges
     for count in range(int(edges)):
         left, right = input().split(' ')
@@ -25,21 +25,33 @@ def dfs(graph, current, visited, color):
     neighbors = [str(_) for _ in graph[current]]
     if current not in visited:
         visited.append(current)
-        color[int(current)] = 1 - color[int(current)-1]
+        if current != '1':
+            color[current] = 1 - color[str(int(current) - 1)]
         for neighbor in neighbors:
             dfs(graph, neighbor, visited, color)
-    elif color[int(current)] == color[int(current) - 1]:
-        flag = "NO"
+            if color[neighbor] == color[current]:
+                flag = "NO"
+
     return visited, color
 
+
+# def bipartite(graph, color):
+#     for vertex in graph:
+#         neighbors = [_ for _ in graph[vertex]]
+#         color_list = [color[neighbor] for neighbor in neighbors]
+#         if color[vertex] in color_list:
+#             return "NO"
+#     return "YES"
 
 def main():
     graph, vertices = create_graph()
     root = '1'
-    color = [0 for _ in range(int(vertices) + 1)]
+    color = {str(_): 0 for _ in range(1, int(vertices) + 1)}
     visited, color = dfs(graph, root, [], color)
-    print(visited)
-    print(color)
+    # print(visited)
+    # print(color)
+    # print(color)
+    # print(bipartite(graph, color))
     print(flag)
 
 
