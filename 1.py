e1b382a09563b545234fe25bf0598ab7cc6d48aa
Varun Shijo <varunshi@buffalo.edu>
2018-02-26 19:44:09 -0500

    Done

--- 1.py
+++ 1.py
@@ -1,6 +1,8 @@
 # Author: Varun Shijo UB 50244968
 # DFS for Graphs
 
+bipartite = "YES"
+
 
 def create_graph():
     vertices, edges = input().split(' ')
@@ -17,9 +19,30 @@ def create_graph():
     return graph, vertices
 
 
+def DFS(vertices, visited, graph, color):
+    for s in vertices:
+        if s not in visited:
+            return dfs(s, graph, visited, color)
+
+
+def dfs(v, graph, visited, color):
+    global bipartite
+    for u in graph[v]:
+        if u not in visited:
+            visited.append(u)
+            color[u] = 1 - color[v]
+            dfs(u, graph, visited, color)
+        elif color[u] == color[v]:
+            bipartite = "NO"
+    return visited, color
+
+
 def main():
     graph, vertices = create_graph()
     print(graph)
+    color = {str(_): 0 for _ in range(1, int(vertices) + 1)}
+    visited, color = DFS(vertices, [], graph, color)
+    print(bipartite)
 
 
 if __name__ == '__main__':
