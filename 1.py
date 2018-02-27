432c0473703120124ea36f2704ce9f7dc3ac0f6e
Varun Shijo <varunshi@buffalo.edu>
2018-02-26 19:33:39 -0500

    getting there

--- 1.py
+++ 1.py
@@ -1,8 +1,6 @@
 # Author: Varun Shijo UB 50244968
 # DFS for Graphs
 
-flag = "YES"
-
 
 def create_graph():
     vertices, edges = input().split(' ')
@@ -19,40 +17,9 @@ def create_graph():
     return graph, vertices
 
 
-def dfs(graph, current, visited, color):
-    # Traversal
-    global flag
-    neighbors = [str(_) for _ in graph[current]]
-    if current not in visited:
-        visited.append(current)
-        if current != '1':
-            color[current] = 1 - color[str(int(current) - 1)]
-        for neighbor in neighbors:
-            dfs(graph, neighbor, visited, color)
-            if color[neighbor] == color[current]:
-                flag = "NO"
-
-    return visited, color
-
-
-# def bipartite(graph, color):
-#     for vertex in graph:
-#         neighbors = [_ for _ in graph[vertex]]
-#         color_list = [color[neighbor] for neighbor in neighbors]
-#         if color[vertex] in color_list:
-#             return "NO"
-#     return "YES"
-
 def main():
     graph, vertices = create_graph()
-    root = '1'
-    color = {str(_): 0 for _ in range(1, int(vertices) + 1)}
-    visited, color = dfs(graph, root, [], color)
-    # print(visited)
-    # print(color)
-    # print(color)
-    # print(bipartite(graph, color))
-    print(flag)
+    print(graph)
 
 
 if __name__ == '__main__':
