1f9beedd15b07d8386c3e2ddfdae00d5109f62cb
Varun Shijo <varunshi@buffalo.edu>
2018-04-22 19:31:58 -0400

    Done

--- 4 Dynamic Longest Common Subsequence/main.py
+++ 4 Dynamic Longest Common Subsequence/main.py
@@ -4,32 +4,34 @@ pi = []
 
 def gen_mat(len_1, len_2):
     global mat, pi
-    mat = [[0 for x in range(len_1 + 1)] for y in range(len_2 + 1)]
-    pi = [[0 for x in range(len_1 + 1)] for y in range(len_2 + 1)]
+    mat = [[0 for x in range(len_2 + 1)] for y in range(len_1 + 1)]
+    pi = [[0 for x in range(len_2 + 1)] for y in range(len_1 + 1)]
 
 
 def lcs(charset_a, charset_b):
     global mat, pi
     len_1 = len(charset_a)
     len_2 = len(charset_b)
-    for i in range(len_1):
-        for j in range(len_2):
-            if charset_a[i] == charset_b[j]:
+    for i in range(1, len_1 + 1):
+        for j in range(1, len_2 + 1):
+            if charset_a[i - 1] == charset_b[j - 1]:
                 mat[i][j] = mat[i - 1][j - 1] + 1
                 pi[i][j] = "d"
             else:
-                if mat[i - 1][j] <= mat[i][j - 1]:
+                left = mat[i - 1][j]
+                right = mat[i][j - 1]
+                if left <= right:
                     mat[i][j] = mat[i][j - 1]
                     pi[i][j] = "l"
                 else:
                     mat[i][j] = mat[i - 1][j]
                     pi[i][j] = "u"
-    i = len_1 - 1
-    j = len_2 - 1
+    i = len_1
+    j = len_2
     s = []
-    while i >= 0 and j >= 0:
+    while i > 0 and j > 0:
         if pi[i][j] == "d":
-            s.insert(0, charset_a[i])
+            s.insert(0, charset_a[i-1])
             i -= 1
             j -= 1
         elif pi[i][j] == "u":
